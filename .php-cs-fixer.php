f61a690fef1d3909cd6c61f05c27c9d5c45b7b04
Michał Rubel <contact@observer.name>
2021-10-30 21:41:48 +0200

    cs fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -21,7 +21,9 @@ return (new PhpCsFixer\Config())
         'trailing_comma_in_multiline' => true,
         'phpdoc_scalar' => true,
         'unary_operator_spaces' => true,
-        'binary_operator_spaces' => true,
+        'binary_operator_spaces' => [
+            'default' => 'align_single_space_minimal',
+        ],
         'blank_line_before_statement' => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
