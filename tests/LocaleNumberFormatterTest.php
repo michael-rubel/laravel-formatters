30dc5508d855b89dce7dcd4c847142dbe8b63b98
Michał Rubel <contact@observer.name>
2021-11-01 12:30:38 +0100

    use special character in tests instead of cleaning the string

--- tests/LocaleNumberFormatterTest.php
+++ tests/LocaleNumberFormatterTest.php
@@ -11,73 +11,49 @@ class LocaleNumberFormatterTest extends TestCase
     /** @test */
     public function testCanFormatNumberBasedOnPlLocale()
     {
-        $result = str_replace(
-            "\xc2\xa0",
-            ' ',
-            format(LocaleNumberFormatter::class, [
-                'number' => 10000,
-                'locale' => 'pl',
-            ])
-        );
+        $result = format(LocaleNumberFormatter::class, [
+            'number' => 10000,
+            'locale' => 'pl',
+        ]);
 
-        $this->assertSame('10 000', $result);
+        $this->assertSame('10 000', $result);
 
-        $result = str_replace(
-            "\xc2\xa0",
-            ' ',
-            format(LocaleNumberFormatter::class, [
-                'number' => 10000.00,
-                'locale' => 'pl',
-            ])
-        );
+        $result = format(LocaleNumberFormatter::class, [
+            'number' => 10000.00,
+            'locale' => 'pl',
+        ]);
 
-        $this->assertSame('10 000', $result);
+        $this->assertSame('10 000', $result);
 
-        $result = str_replace(
-            "\xc2\xa0",
-            ' ',
-            format(LocaleNumberFormatter::class, [
-                'number' => 10000.50,
-                'locale' => 'pl',
-            ])
-        );
+        $result = format(LocaleNumberFormatter::class, [
+            'number' => 10000.50,
+            'locale' => 'pl',
+        ]);
 
-        $this->assertSame('10 000,5', $result);
+        $this->assertSame('10 000,5', $result);
     }
 
     /** @test */
     public function testCanFormatNumberBasedOnEnLocale()
     {
-        $result = str_replace(
-            "\xc2\xa0",
-            ' ',
-            format(LocaleNumberFormatter::class, [
-                'number' => 10000,
-                'locale' => 'en',
-            ])
-        );
+        $result = format(LocaleNumberFormatter::class, [
+            'number' => 10000,
+            'locale' => 'en',
+        ]);
 
         $this->assertSame('10,000', $result);
 
-        $result = str_replace(
-            "\xc2\xa0",
-            ' ',
-            format(LocaleNumberFormatter::class, [
-                'number' => 10000.00,
-                'locale' => 'en',
-            ])
-        );
+        $result = format(LocaleNumberFormatter::class, [
+            'number' => 10000.00,
+            'locale' => 'en',
+        ]);
 
         $this->assertSame('10,000', $result);
 
-        $result = str_replace(
-            "\xc2\xa0",
-            ' ',
-            format(LocaleNumberFormatter::class, [
-                'number' => 10000.50,
-                'locale' => 'en',
-            ])
-        );
+        $result = format(LocaleNumberFormatter::class, [
+            'number' => 10000.50,
+            'locale' => 'en',
+        ]);
 
         $this->assertSame('10,000.5', $result);
     }
