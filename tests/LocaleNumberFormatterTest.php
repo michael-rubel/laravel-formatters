d7e1e6d351c757f18ab39c38c1dd659b6b50831c
Michał Rubel <contact@observer.name>
2021-11-09 20:35:25 +0100

    let the tests pass

--- tests/LocaleNumberFormatterTest.php
+++ tests/LocaleNumberFormatterTest.php
@@ -5,6 +5,7 @@ namespace MichaelRubel\Formatters\Tests;
 use Carbon\Carbon;
 use MichaelRubel\Formatters\Collection\DateTimeFormatter;
 use MichaelRubel\Formatters\Collection\LocaleNumberFormatter;
+use NumberFormatter;
 
 class LocaleNumberFormatterTest extends TestCase
 {
@@ -94,34 +95,34 @@ class LocaleNumberFormatterTest extends TestCase
             'locale' => 'fr',
         ]);
 
-        $this->assertEquals('10 000,00', $result);
+        $this->assertSame('10 000,00', $result);
 
         $result = format(LocaleNumberFormatter::class, [
             'number' => 10000.00,
             'locale' => 'fr',
         ]);
 
-        $this->assertSame('10 000,00', $result);
+        $this->assertSame('10 000,00', $result);
 
         $result = format(LocaleNumberFormatter::class, [
             'number' => 10000.50,
             'locale' => 'fr',
         ]);
 
-        $this->assertSame('10 000,50', $result);
+        $this->assertSame('10 000,50', $result);
 
         $result = format(LocaleNumberFormatter::class, [
             'number' => 10000.5549,
             'locale' => 'fr',
         ]);
 
-        $this->assertSame('10 000,55', $result);
+        $this->assertSame('10 000,55', $result);
 
         $result = format(LocaleNumberFormatter::class, [
             'number' => 10000.5550,
             'locale' => 'fr',
         ]);
 
-        $this->assertSame('10 000,56', $result);
+        $this->assertSame('10 000,56', $result);
     }
 }
