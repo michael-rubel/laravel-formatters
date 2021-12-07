f9fa4954565bc182422b8e054c2b4631dcd8eb6a
Michael Rubel <contact@observer.name>
2021-12-07 14:38:29 +0100

    more tests

--- tests/MaskEmailFormatterTest.php
+++ tests/MaskEmailFormatterTest.php
@@ -10,11 +10,15 @@ class MaskEmailFormatterTest extends TestCase
     public function testCanFormatAsMaskedEmail()
     {
         $format = format(MaskStringFormatter::class, 'MyMaskedString');
-
         $this->assertSame('MyMa******ring', $format);
 
         $format = format(MaskStringFormatter::class, 'test@example.com');
-
         $this->assertSame('test********.com', $format);
+
+        $format = format(MaskStringFormatter::class, 'testTestT');
+        $this->assertSame('test*estT', $format);
+
+        $format = format(MaskStringFormatter::class, 'tes');
+        $this->assertSame('tes', $format);
     }
 }
