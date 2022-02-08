371d778019527b39d310fa0cd9a5efadcd6dda4f
Olsza <olsza@users.noreply.github.com>
2022-02-08 14:24:32 +0100

    add Tax Number Formatter

---
+++ src/Collection/TaxNumberFormatter.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MichaelRubel\Formatters\Collection;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+use MichaelRubel\Formatters\Formatter;
+use function Webmozart\Assert\Tests\StaticAnalysis\length;
+use function Webmozart\Assert\Tests\StaticAnalysis\nullOrUpper;
+use function Webmozart\Assert\Tests\StaticAnalysis\upper;
+
+class TaxNumberFormatter implements Formatter
+{
+    /**
+     * @var string
+     */
+    public string $key_number = 'tax_number';
+
+    /**
+     * @var string
+     */
+    public string $key_country = 'country';
+
+    /**
+     * Format the Tax Number.
+     *
+     * @param Collection $items
+     *
+     * @return string
+     */
+    public function format(Collection $items): string
+    {
+        $tax_number = preg_replace(
+            '/[^\d\w]/',
+            '',
+            $items->get($this->key_number, '')
+        );
+
+        if (empty($items->get($this->key_country))) {
+            return $tax_number;
+        }
+
+        $country = strtoupper($items->get($this->key_country));
+
+        $prefix = strtoupper(
+            substr(
+                $tax_number,
+                0,
+                strlen($country)
+            )
+        );
+
+        return $prefix === $country
+            ? $country . substr(
+                $tax_number,
+                strlen($country)
+            )
+            : $country . $tax_number;
+    }
+}
