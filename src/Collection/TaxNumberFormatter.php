011417e387266884b533b537809a9d5c1b0975ed
Olsza <olsza@users.noreply.github.com>
2022-02-09 10:20:19 +0100

    refactoring - extract blocks of code to methods

--- src/Collection/TaxNumberFormatter.php
+++ src/Collection/TaxNumberFormatter.php
@@ -28,31 +28,70 @@ class TaxNumberFormatter implements Formatter
      */
     public function format(Collection $items): string
     {
-        $tax_number = preg_replace(
-            '/[^\d\w]/',
-            '',
-            $items->get($this->key_number, '')
-        );
+        $tax_number = $this->getCleanTaxNumber($items);
 
         if (empty($items->get($this->key_country))) {
             return $tax_number;
         }
 
-        $country = strtoupper($items->get($this->key_country));
+        $country = $this->getCountry($items);
 
-        $prefix = strtoupper(
+        $prefix = $this->getPrefix($tax_number, $country);
+
+        return $this->getFullTaxNumber($prefix, $country, $tax_number);
+    }
+
+    /**
+     * @param Collection $items
+     * @return string
+     */
+    private function getCountry(Collection $items): string
+    {
+        return strtoupper($items->get($this->key_country));
+    }
+
+    /**
+     * @param string $tax_number
+     * @param string $country
+     * @return string
+     */
+    private function getPrefix(string $tax_number, string $country): string
+    {
+        return strtoupper(
             substr(
-                $tax_number,
+                (string) $tax_number,
                 0,
                 strlen($country)
             )
         );
+    }
 
+    /**
+     * @param string $prefix
+     * @param string $country
+     * @param string $tax_number
+     * @return string
+     */
+    private function getFullTaxNumber(string $prefix, string $country, string $tax_number): string
+    {
         return $prefix === $country
             ? $country . substr(
                 $tax_number,
                 strlen($country)
             )
             : $country . $tax_number;
+    }
+
+    /**
+     * @param Collection $items
+     * @return string
+     */
+    private function getCleanTaxNumber(Collection $items): string
+    {
+        return (string) preg_replace(
+            '/[^\d\w]/',
+            '',
+            $items->get($this->key_number, '')
+        );
     }
 }
