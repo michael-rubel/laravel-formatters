264a1f8179b49d0a693afd8015151c209a814605
Michael Rubél <contact@observer.name>
2022-08-16 13:41:16 +0200

    pass without country

--- tests/TaxNumberFormatterTest.php
+++ tests/TaxNumberFormatterTest.php
@@ -182,5 +182,8 @@ class TaxNumberFormatterTest extends TestCase
 
         $result = format(TaxNumberFormatter::class, ' 526- -10 -40- 567 ', 'PL');
         $this->assertSame('PL5261040567', $result);
+
+        $result = format(TaxNumberFormatter::class, ' 526- -10 -40- 567 ');
+        $this->assertSame('5261040567', $result);
     }
 }
