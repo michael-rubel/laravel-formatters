82e57642f071817f09b2e2e350c45e89e8e1d3e0
Michael Rubél <contact@observer.name>
2022-09-20 12:04:18 +0200

    add fullname formatter tests

---
+++ tests/FullNameFormatterTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace MichaelRubel\Formatters\Tests;
+
+use MichaelRubel\Formatters\Collection\FullNameFormatter;
+
+class FullNameFormatterTest extends TestCase
+{
+    public function testCanUseStringBinding()
+    {
+        $name = format('full-name', 'michael');
+
+        $this->assertSame('Michael', $name);
+    }
+
+    public function testCanFormatFirstName()
+    {
+        $name = format(FullNameFormatter::class, 'michael');
+
+        $this->assertSame('Michael', $name);
+    }
+
+    public function testCanFormatLastName()
+    {
+        $name = format(FullNameFormatter::class, 'rubél');
+
+        $this->assertSame('Rubél', $name);
+    }
+
+    public function testCanFormatFirstAndLastName()
+    {
+        $name = format(FullNameFormatter::class, 'michaeL rubéL');
+
+        $this->assertSame('Michael Rubél', $name);
+    }
+
+    public function testAddsSpaceBetweenWords()
+    {
+        $name = format(FullNameFormatter::class, 'MichaelRubél');
+
+        $this->assertSame('Michael Rubél', $name);
+    }
+
+    public function testCutsSpaceBeforeAndAfter()
+    {
+        $name = format(FullNameFormatter::class, ' MichaelRubél ');
+
+        $this->assertSame('Michael Rubél', $name);
+    }
+
+    public function testEmptyStringAndWeirdSymbols()
+    {
+        $name = format(FullNameFormatter::class, '');
+        $this->assertSame('', $name);
+
+        $name = format(FullNameFormatter::class, '   ');
+        $this->assertSame('', $name);
+
+        $name = format(FullNameFormatter::class, '  ...  ');
+        $this->assertSame('...', $name);
+
+        $name = format(FullNameFormatter::class, '⠀');
+        $this->assertSame('⠀', $name);
+
+        $name = format(FullNameFormatter::class, ' ');
+        $this->assertSame('', $name);
+    }
+}
