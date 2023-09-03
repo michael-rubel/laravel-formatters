9a9c386d2528672df000cb11fcecfa407d4b897a
514sid <140138716+514sid@users.noreply.github.com>
2023-09-03 11:43:12 +0400

    formatting

--- src/Collection/IntFormatter.php
+++ src/Collection/IntFormatter.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace MichaelRubel\Formatters\Collection;
 
-use Num\Num;
 use MichaelRubel\Formatters\Formatter;
+use Num\Num;
 
 class IntFormatter implements Formatter
 {
     /**
-     * @param int|float|string|null $value The value to format.
-     * @param string|null $decimal_separator The decimal separator to use for formatting.
+     * @param  int|float|string|null  $value The value to format.
+     * @param  string|null  $decimal_separator The decimal separator to use for formatting.
      */
     public function __construct(
         public int|float|string|null $value = null,
@@ -22,7 +22,7 @@ class IntFormatter implements Formatter
     /**
      * Get the valid decimal separator or null.
      *
-     * @param string|null $decimal_separator The decimal separator to convert.
+     * @param  string|null  $decimal_separator The decimal separator to convert.
      * @return string|null The valid decimal separator or null.
      */
     private function getDecimalSeparator(?string $decimal_separator): ?string
